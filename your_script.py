1ed8533c6ee9464e35369a831b2e023667e1856f
hamazinger <138296140+hamazinger@users.noreply.github.com>
2023-09-06 00:50:05 +0900

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -174,53 +174,6 @@ def main():
 
         
 
-        #----------以下不要--------------
-
-        # # プロット
-        # fig, ax1 = plt.subplots()
-        
-        # color = 'tab:red'
-        # ax1.set_xlabel('Time (Quarterly)')
-        # ax1.set_ylabel('Article Counts', color=color)
-        
-        # # プロットのデータポイント数
-        # n_plot_points = 10000
-        # xnew = np.linspace(df_trends_quarterly.index.astype(int).min(), df_trends_quarterly.index.astype(int).max(), n_plot_points)
-        
-        # if not df_articles.empty:
-        #     df_articles['date'] = pd.to_datetime(df_articles['date'])
-        #     df_articles.set_index('date', inplace=True)
-        #     df_articles_quarterly = df_articles.resample('3M').count()
-        
-        #     # 近似曲線の描画
-        #     # スプライン補間関数の生成（日時をエポック秒に変換）と近似曲線の値を生成
-        #     # spl_articles = make_interp_spline(df_articles_quarterly.index.astype(int), df_articles_quarterly)
-        #     # ynew_articles = spl_articles(xnew)
-        #     # 記事数の近似曲線の描画（エポック秒を日時に戻す）
-        #     # ax1.plot(pd.to_datetime(xnew), ynew_articles, color=color)
-        #     ax1.plot(df_articles_quarterly.index, df_articles_quarterly, color=color)
-        #     # 目盛り幅を設定します。
-        #     # ax1.yaxis.set_ticks(np.arange(0, df_articles_quarterly.max() + 1, 1))
-        #     ax1.yaxis.set_ticks(np.arange(0, df_articles_quarterly.iloc[:, 0].max() + 1, 1))
-        
-        # ax1.tick_params(axis='y', labelcolor=color)
-        
-        # # x軸の日付のフォーマットをyyyyに変更
-        # ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
-        
-        # ax2 = ax1.twinx()
-        # color = 'tab:blue'
-        # ax2.set_ylabel('Google Trends', color=color)
-    
-        
-        # ax2.plot(df_trends_quarterly.index, df_trends_quarterly, color=color)
-        # ax2.tick_params(axis='y', labelcolor=color)
-        # fig.tight_layout()
-        # st.pyplot(fig)
-        # if not df_articles.empty:
-        #     st.dataframe(df_articles.reset_index())
-        # st.write(f"Related terms: {', '.join(related_terms)}")
-
 
         #-----------セミナー結果出力----------------------
         query = f"""
